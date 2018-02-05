b6eef16d5a288704e8ddc630fe6561f0bbba2dbf
Ce28 <christian.sapien@hotmail.com>
2018-02-05 00:42:25 -0700

    Se realizo el tutorial 3 del curso

---
+++ Tutorial03/Pro02.HolaMundo/Pro02.HolaMundo/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+namespace Pro02.HolaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Comentario de 1 linea.
+
+
+            /*
+             * 
+             *Comentario de multiples lineas. 
+             * 
+             * 
+             */
+
+
+
+            //Este metodo imprime hola mundo en pantalla.
+            System.Console.WriteLine("Hola Mundo");
+
+            System.Console.ReadKey();
+        }
+    }
+}
