0de579ef3d5f9cde49bee96474a161be646b30ad
Ce28 <christian.sapien@hotmail.com>
2018-02-05 11:42:58 -0700

    Se realizo el tutorial 9 del curso

---
+++ Tutorial09/trycatch/trycatch/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trycatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double precio, total;
+            int cantidad;
+                        
+            //try ..... catch
+            try
+            {
+                Console.WriteLine("Cantidad: ");
+                cantidad = Convert.ToInt16(Console.ReadLine());
+
+                Console.WriteLine("Precio: ");
+                precio = Convert.ToDouble(Console.ReadLine());
+
+                total = cantidad * precio;
+
+                Console.WriteLine("El total es: $" + total);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ha ocurrido un error: " + ex.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
