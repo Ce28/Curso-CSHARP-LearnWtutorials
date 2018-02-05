140f15679ed10bef95cd7591078e4bd968fe0cd8
Ce28 <christian.sapien@hotmail.com>
2018-02-05 10:55:33 -0700

    Se realizo el tutorial 7 del curso

---
+++ Tutorial07/Operadores/Operadores/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operadores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Operadores relacionales.
+            /*  > Mayor que
+             *  < Menor que
+             *  >= Mayor o igual que
+             *  <= Menor o igual que
+             *  == es igual
+             *  != es diferente
+             */
+
+            //Operadores logicos.
+            /*  && and
+             *  || or
+             *  !  not
+             *  == es igual
+             *  != es diferente
+             */ 
+
+            double peso;
+            byte edad;
+
+            Console.WriteLine("Digita tu peso: ");
+            peso = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digita tu edad: ");
+            edad = Convert.ToByte(Console.ReadLine());
+
+            Console.Clear();
+
+            if (peso < 100 && edad >= 15)
+            {
+                Console.WriteLine("Tu peso es normal");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
