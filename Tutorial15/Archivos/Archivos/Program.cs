725c50b6be41f028bf32604c1f00b1ee7c311049
Ce28 <christian.sapien@hotmail.com>
2018-02-05 15:25:43 -0700

    Se realizo el tutorial 15 del curso

---
+++ Tutorial15/Archivos/Archivos/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+namespace Archivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TextWriter archivo;         //Variable de tipo archivo.
+            archivo = new StreamWriter("archivo.txt");  //Se crea una instancia (objeto) de ella.
+
+            string mensaje;                 //Se inicializa una variable que guardara la linea.
+            mensaje = Console.ReadLine();   //Se guarda la linea.
+
+            archivo.WriteLine(mensaje);     //Escribe en el archivo.
+
+            archivo.Close();                //Se cierra y guarda el archivo
+
+            Console.Clear();                //Se limpia la pantalla de comando.
+
+            Console.WriteLine("Se ha guardado el archivo...");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Tutorial15/Archivos/Archivos/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general sobre un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos atributos para modificar la información
+// asociada con un ensamblado.
+[assembly: AssemblyTitle("Archivos")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Archivos")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible como false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si necesita obtener acceso a un tipo de este ensamblado desde 
+// COM, establezca el atributo ComVisible como true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como identificador de typelib si este proyecto se expone a COM
+[assembly: Guid("c6c08b80-739b-4cfa-a0dc-ebb524277917")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria 
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o establecer como predeterminados los números de compilación y de revisión 
+// mediante el carácter '*', como se muestra a continuación:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
