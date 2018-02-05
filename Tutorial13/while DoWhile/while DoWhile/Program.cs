0c19222a4519cacaa0a8262f65d28231427e7018
Ce28 <christian.sapien@hotmail.com>
2018-02-05 13:14:03 -0700

    Se realizo el tutorial 13 del curso

---
+++ Tutorial13/while DoWhile/while DoWhile/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace while_DoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //while.
+            //bool encender = true;
+            //while (encender)
+            //{
+            //    Console.WriteLine("Hola a todos");  //Bucle infinito
+            //}
+
+
+            //DoWhile.
+            bool encender = false;
+            do
+            {
+                Console.WriteLine("Hola a todos");  //Bucle infinito
+            } while (encender);
+
+            Console.ReadKey();
+        }
+    }
+}
