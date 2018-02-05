71b642333b2c0e907c691a9e224a2ce56636cd1c
Ce28 <christian.sapien@hotmail.com>
2018-02-05 00:38:15 -0700

    Se realizo el tutorial 2 del curso

---
+++ Tutorial02/Pro02.HolaMundo/Pro02.HolaMundo/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace Pro02.HolaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hola Mundo");
+
+            System.Console.ReadKey();
+        }
+    }
+}
