3c203f821703a6cd3075750bf5e09809d3218a10
Ce28 <christian.sapien@hotmail.com>
2018-02-05 10:27:42 -0700

    Se realizo el tutorial 5 del curso

---
+++ Tutorial05/OperadoresAsignacion/OperadoresAsignacion/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperadoresAsignacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte edad = 20;
+
+            //Se puede aser de esta manera o usando operadores de asignacion
+            //edad = Convert.ToByte(edad + 10);
+            //Operadores de asignacion      =, +=, -=, *=, /=
+            edad += 10;
+
+            Console.WriteLine("Tu edad es : " + edad);
+
+            Console.ReadKey();
+        }
+    }
+}
