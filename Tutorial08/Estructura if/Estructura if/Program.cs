720cc299278fb2dcf0ee4513f4f343869d2aeb11
Ce28 <christian.sapien@hotmail.com>
2018-02-05 11:02:22 -0700

    Se realizo el tutorial 8 del curso

---
+++ Tutorial08/Estructura if/Estructura if/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estructura_if
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Estructura if,else y anidada.
+            byte juan = 19, pedro = 19;
+
+            if (juan > pedro)
+            {
+                Console.WriteLine("Juan es mayor que pedro");
+            }
+            else if (juan < pedro)
+            {
+                Console.WriteLine("Juan es menor que pedro");
+            }
+            else
+            {
+                Console.WriteLine("Juan y pedro tienen la misma edad");
+            }
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
