e71d81018bde048479fd0abbaa981a6fd7d00421
Ce28 <christian.sapien@hotmail.com>
2018-02-05 17:53:52 -0700

    Se realizo el tutorial 20 del curso

---
+++ Tutorial20/Herencias/Herencias/ClasePadre.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herencias
+{
+    class ClasePadre
+    {
+        //Metodos
+        public void Saludar()
+        {
+            Console.WriteLine("Hola");
+        }
+
+        public void Despedirse()
+        {
+            Console.WriteLine("Adios...");
+        }
+    }
+}
---
+++ Tutorial20/Herencias/Herencias/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herencias
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClaseHija Heren = new ClaseHija();  //Crea un objeto de la ClaseHija que hereda de la ClasePadre.
+            Heren.Saludar();                    //Se puede utilizar los metodos heredados.
+            Heren.Despedirse();                 //Igual aqui.
+
+            Console.ReadKey();
+        }
+    }
+}
