ac6a84e6ee115685828c671faaadf2dfbf37149c
Ce28 <christian.sapien@hotmail.com>
2018-02-06 22:06:52 -0700

    Se realizo el tutorial 32 del curso

---
+++ Tutorial32/FactuxD/FactuxD/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Data.SqlClient;
+using MiLibreria;
+
+namespace FactuxD
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Utilidades.Ejecutar("SELECT * FROM Clientes WHERE id = 1");
+        }
+    }
+}
