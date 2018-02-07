d80ec4030282dd0570501a6c946a57b91f0656e6
Ce28 <christian.sapien@hotmail.com>
2018-02-06 23:51:31 -0700

    Se realizo el tutorial 35 del curso

---
+++ Tutorial35/FactuxD/FactuxD/VentanaAdmin.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using MiLibreria;
+
+namespace FactuxD
+{
+    public partial class VentanaAdmin : Form
+    {
+        public VentanaAdmin()
+        {
+            InitializeComponent();
+        }
+
+        private void VentanaAdmin_Load(object sender, EventArgs e)
+        {
+            string cmd = "SELECT * FROM Usuarios WHERE id_usuario = " + VentanaLogin.Codigo;
+            DataSet DS = Utilidades.Ejecutar(cmd);
+
+            lblNomAdm.Text = DS.Tables[0].Rows[0]["Nom_usu"].ToString();
+            lblUsAdm.Text = DS.Tables[0].Rows[0]["account"].ToString();
+            lblCodAdm.Text = DS.Tables[0].Rows[0]["id_usuario"].ToString();
+
+            string url = DS.Tables[0].Rows[0]["Foto"].ToString();
+
+            pbFoto.Image = Image.FromFile(url);
+        }
+
+        private void VentanaAdmin_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+
+
+    }
+}
---
+++ Tutorial35/FactuxD/FactuxD/VentanaLogin.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Data.SqlClient;
+using MiLibreria;
+
+namespace FactuxD
+{
+    public partial class VentanaLogin : Form
+    {
+        public VentanaLogin()
+        {
+            InitializeComponent();
+        }
+
+        public static string Codigo = "";
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string CMD = string.Format("SELECT * FROM Usuarios WHERE account = '{0}' AND password = '{1}'", txtUsuario.Text.Trim(), txtContraseña.Text.Trim());
+                DataSet ds = Utilidades.Ejecutar(CMD);
+
+                Codigo = ds.Tables[0].Rows[0]["id_usuario"].ToString().Trim();
+
+                string cuenta = ds.Tables[0].Rows[0]["account"].ToString().Trim();
+                string contra = ds.Tables[0].Rows[0]["password"].ToString().Trim();
+                
+                if (cuenta == txtUsuario.Text.Trim() && contra == txtContraseña.Text.Trim())
+                {
+                    if (Convert.ToBoolean(ds.Tables[0].Rows[0]["Status_admin"]) == true)
+                    {
+                        VentanaAdmin VenAd = new VentanaAdmin();
+                        this.Hide();
+                        VenAd.Show();
+                    }
+                    else
+                    {
+                        VentanaUser VenUs = new VentanaUser();
+                        this.Hide();
+                        VenUs.Show();
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Usuario o contraseña incorrecta!...");
+            }
+        }
+
+        private void VentanaLogin_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+
+    }
+}
