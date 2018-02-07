3ba474345cf7c4dd1fdebfa4bebbd024237502f2
Ce28 <christian.sapien@hotmail.com>
2018-02-06 21:45:47 -0700

    Se realizo el tutorial 31 del curso

---
+++ Tutorial31/FactuxD/FactuxD/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Data.SqlClient;
+
+namespace FactuxD
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection Con = new SqlConnection("Data Source=.;Initial Catalog=Administracion;Integrated Security=True");
+                Con.Open();
+                MessageBox.Show("Se a conectado");
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show("Ha ocurrido un error: " + error.Message);
+            }
+        }
+    }
+}
