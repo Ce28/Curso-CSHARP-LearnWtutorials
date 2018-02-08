072094f89e62c6cc670928ffad84605497b49792
Ce28 <christian.sapien@hotmail.com>
2018-02-07 22:08:59 -0700

    Se realizo el tutorial 36 del curso

---
+++ Tutorial36/FactuxD/FactuxD/VentanaAdmin.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace FactuxD
+{
+    partial class VentanaAdmin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblNomAdm = new System.Windows.Forms.Label();
+            this.btnContenedorPri = new System.Windows.Forms.Button();
+            this.btnAdministrarUsu = new System.Windows.Forms.Button();
+            this.btnCambiarContrasena = new System.Windows.Forms.Button();
+            this.btnCerrarSesion = new System.Windows.Forms.Button();
+            this.lblUsAdm = new System.Windows.Forms.Label();
+            this.lblCodAdm = new System.Windows.Forms.Label();
+            this.pbFoto = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbFoto)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(287, 276);
+            this.btnSalir.Size = new System.Drawing.Size(140, 40);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Admin:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Usuario:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 164);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Código:";
+            // 
+            // lblNomAdm
+            // 
+            this.lblNomAdm.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblNomAdm.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNomAdm.Location = new System.Drawing.Point(57, 16);
+            this.lblNomAdm.Name = "lblNomAdm";
+            this.lblNomAdm.Size = new System.Drawing.Size(135, 33);
+            this.lblNomAdm.TabIndex = 3;
+            this.lblNomAdm.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnContenedorPri
+            // 
+            this.btnContenedorPri.Location = new System.Drawing.Point(287, 16);
+            this.btnContenedorPri.Name = "btnContenedorPri";
+            this.btnContenedorPri.Size = new System.Drawing.Size(140, 40);
+            this.btnContenedorPri.TabIndex = 6;
+            this.btnContenedorPri.Text = "Contenedor Principal";
+            this.btnContenedorPri.UseVisualStyleBackColor = true;
+            // 
+            // btnAdministrarUsu
+            // 
+            this.btnAdministrarUsu.Location = new System.Drawing.Point(287, 81);
+            this.btnAdministrarUsu.Name = "btnAdministrarUsu";
+            this.btnAdministrarUsu.Size = new System.Drawing.Size(140, 40);
+            this.btnAdministrarUsu.TabIndex = 7;
+            this.btnAdministrarUsu.Text = "Administrar Usuarios";
+            this.btnAdministrarUsu.UseVisualStyleBackColor = true;
+            // 
+            // btnCambiarContrasena
+            // 
+            this.btnCambiarContrasena.Location = new System.Drawing.Point(287, 146);
+            this.btnCambiarContrasena.Name = "btnCambiarContrasena";
+            this.btnCambiarContrasena.Size = new System.Drawing.Size(140, 40);
+            this.btnCambiarContrasena.TabIndex = 8;
+            this.btnCambiarContrasena.Text = "Cambiar Contraseña";
+            this.btnCambiarContrasena.UseVisualStyleBackColor = true;
+            // 
+            // btnCerrarSesion
+            // 
+            this.btnCerrarSesion.Location = new System.Drawing.Point(287, 211);
+            this.btnCerrarSesion.Name = "btnCerrarSesion";
+            this.btnCerrarSesion.Size = new System.Drawing.Size(140, 40);
+            this.btnCerrarSesion.TabIndex = 9;
+            this.btnCerrarSesion.Text = "Cerrar Sesión";
+            this.btnCerrarSesion.UseVisualStyleBackColor = true;
+            // 
+            // lblUsAdm
+            // 
+            this.lblUsAdm.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblUsAdm.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsAdm.Location = new System.Drawing.Point(57, 85);
+            this.lblUsAdm.Name = "lblUsAdm";
+            this.lblUsAdm.Size = new System.Drawing.Size(135, 33);
+            this.lblUsAdm.TabIndex = 12;
+            this.lblUsAdm.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblCodAdm
+            // 
+            this.lblCodAdm.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblCodAdm.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCodAdm.Location = new System.Drawing.Point(57, 154);
+            this.lblCodAdm.Name = "lblCodAdm";
+            this.lblCodAdm.Size = new System.Drawing.Size(135, 33);
+            this.lblCodAdm.TabIndex = 13;
+            this.lblCodAdm.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pbFoto
+            // 
+            this.pbFoto.Location = new System.Drawing.Point(12, 190);
+            this.pbFoto.Name = "pbFoto";
+            this.pbFoto.Size = new System.Drawing.Size(206, 199);
+            this.pbFoto.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbFoto.TabIndex = 11;
+            this.pbFoto.TabStop = false;
+            // 
+            // VentanaAdmin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(444, 421);
+            this.Controls.Add(this.lblCodAdm);
+            this.Controls.Add(this.lblUsAdm);
+            this.Controls.Add(this.pbFoto);
+            this.Controls.Add(this.btnCerrarSesion);
+            this.Controls.Add(this.btnCambiarContrasena);
+            this.Controls.Add(this.btnAdministrarUsu);
+            this.Controls.Add(this.btnContenedorPri);
+            this.Controls.Add(this.lblNomAdm);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "VentanaAdmin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "VentanaAdmin";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.VentanaAdmin_FormClosed);
+            this.Load += new System.EventHandler(this.VentanaAdmin_Load);
+            this.Controls.SetChildIndex(this.label1, 0);
+            this.Controls.SetChildIndex(this.label2, 0);
+            this.Controls.SetChildIndex(this.label3, 0);
+            this.Controls.SetChildIndex(this.lblNomAdm, 0);
+            this.Controls.SetChildIndex(this.btnContenedorPri, 0);
+            this.Controls.SetChildIndex(this.btnAdministrarUsu, 0);
+            this.Controls.SetChildIndex(this.btnCambiarContrasena, 0);
+            this.Controls.SetChildIndex(this.btnCerrarSesion, 0);
+            this.Controls.SetChildIndex(this.pbFoto, 0);
+            this.Controls.SetChildIndex(this.lblUsAdm, 0);
+            this.Controls.SetChildIndex(this.lblCodAdm, 0);
+            this.Controls.SetChildIndex(this.btnSalir, 0);
+            ((System.ComponentModel.ISupportInitialize)(this.pbFoto)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblNomAdm;
+        private System.Windows.Forms.Button btnContenedorPri;
+        private System.Windows.Forms.Button btnAdministrarUsu;
+        private System.Windows.Forms.Button btnCambiarContrasena;
+        private System.Windows.Forms.Button btnCerrarSesion;
+        private System.Windows.Forms.PictureBox pbFoto;
+        private System.Windows.Forms.Label lblUsAdm;
+        private System.Windows.Forms.Label lblCodAdm;
+    }
+}
---
+++ Tutorial36/FactuxD/MiLibreria/Utilidades.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+using System.Data.SqlClient;
+
+namespace MiLibreria
+{
+    public class Utilidades
+    {
+        public static DataSet Ejecutar(string cmd)
+        {
+            SqlConnection Con = new SqlConnection("Data Source=.;Initial Catalog=Administracion;Integrated Security=True");
+            Con.Open();
+
+            DataSet DS = new DataSet();
+            SqlDataAdapter DP = new SqlDataAdapter(cmd, Con);
+
+            DP.Fill(DS);
+
+            Con.Close();
+
+            return DS;
+        }
+    }
+}
