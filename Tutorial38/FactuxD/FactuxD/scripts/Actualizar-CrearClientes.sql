1455f924f32fe8bd70bf651659cd89cd6b558b29
Ce28 <christian.sapien@hotmail.com>
2018-02-08 21:43:18 -0700

    Se realizo el tutorial 38 del curso

---
+++ Tutorial38/FactuxD/FactuxD/scripts/Actualizar-CrearClientes.sql
@@ -0,0 +1,15 @@
+USE Administracion
+GO
+
+CREATE PROCEDURE ActualizaClientes
+@Id_cli INT,
+@Nom_cli VARCHAR(100),
+@Ape_cli VARCHAR(100)
+AS
+
+--Actualiza clientes
+
+IF NOT EXISTS (SELECT id_clientes FROM Cliente WHERE id_clientes = @Id_cli)
+INSERT INTO Cliente(id_clientes, Nom_cli, Ape_cli) VALUES (@Id_cli, @Nom_cli, @Ape_cli)
+ELSE
+UPDATE Cliente SET id_clientes = @Id_cli, Nom_cli= @Nom_cli, Ape_cli = @Ape_cli
