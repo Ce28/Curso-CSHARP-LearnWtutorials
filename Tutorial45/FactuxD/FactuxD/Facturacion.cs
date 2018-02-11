560155ac992b0331137f6c54295216002f365535
Ce28 <christian.sapien@hotmail.com>
2018-02-11 13:53:39 -0700

    Se realizo el tutorial 45 del curso

---
+++ Tutorial45/FactuxD/FactuxD/Facturacion.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using MiLibreria;
+
+namespace FactuxD
+{
+    public partial class Facturacion : Procesos
+    {
+        public Facturacion()
+        {
+            InitializeComponent();
+        }
+
+        private void Facturacion_Load(object sender, EventArgs e)
+        {
+            string cmd = "SELECT * FROM Usuarios WHERE id_usuario = " + VentanaLogin.Codigo;
+
+            DataSet ds;
+            ds = Utilidades.Ejecutar(cmd);
+
+            lblLeAtiende.Text = ds.Tables[0].Rows[0]["Nom_usu"].ToString().Trim();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtCodigoCli.Text.Trim()) == false)
+            {
+                try
+                {
+                    string cmd = string.Format("select Nom_cli from cliente where id_clientes = '{0}'", txtCodigoCli.Text.Trim());
+
+                    DataSet ds = Utilidades.Ejecutar(cmd);
+                    txtCliente.Text = ds.Tables[0].Rows[0]["Nom_cli"].ToString().Trim();
+                    txtCodigoPro.Focus();
+                }
+                catch (Exception error)
+                {
+                    MessageBox.Show("Ha ocurrido nun error: " + error.Message);
+                }
+            }
+        }
+
+        public static int cont_fila = 0;
+        private void btnColocar_Click(object sender, EventArgs e)
+        {
+            if (Utilidades.ValidarFormulario(this, errorProvider1) == false)
+            {
+                bool existe = false;
+                int num_fila = 0;
+
+                if (cont_fila == 0)
+                {
+                    dataGridView1.Rows.Add(txtCodigoPro.Text, txtDescrip.Text, txtPrecio.Text, txtCantidad.Text);
+                    double importe = Convert.ToDouble(dataGridView1.Rows[cont_fila].Cells[2].Value) * Convert.ToDouble(dataGridView1.Rows[cont_fila].Cells[3].Value);
+                    dataGridView1.Rows[cont_fila].Cells[4].Value = importe;
+                    cont_fila++;
+                }
+                else
+                {
+                    foreach (DataGridViewRow Fila in dataGridView1.Rows)
+                    {
+                        if (Fila.Cells[0].Value.ToString() == txtCodigoPro.Text)
+                        {
+                            existe = true;
+                            num_fila = Fila.Index;
+                        }
+                    }
+                    if (existe == true)
+                    {
+                        dataGridView1.Rows[num_fila].Cells[3].Value = (Convert.ToDouble(txtCantidad.Text) + Convert.ToDouble(dataGridView1.Rows[num_fila].Cells[3].Value)).ToString();
+                        double importe = Convert.ToDouble(dataGridView1.Rows[num_fila].Cells[2].Value) * Convert.ToDouble(dataGridView1.Rows[num_fila].Cells[3].Value);
+                        dataGridView1.Rows[num_fila].Cells[4].Value = importe;
+                    }
+                    else
+                    {
+                        dataGridView1.Rows.Add(txtCodigoPro.Text, txtDescrip.Text, txtPrecio.Text, txtCantidad.Text);
+                        double importe = Convert.ToDouble(dataGridView1.Rows[cont_fila].Cells[2].Value) * Convert.ToDouble(dataGridView1.Rows[cont_fila].Cells[3].Value);
+                        dataGridView1.Rows[cont_fila].Cells[4].Value = importe;
+                        cont_fila++;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Tutorial45/FactuxD/MiLibreria/ErrorTxtBox.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MiLibreria
+{
+    public partial class ErrorTxtBox : TextBox
+    {
+        public ErrorTxtBox()
+        {
+            InitializeComponent();
+        }
+
+        /// Texto
+        public Boolean Validar
+        {
+            
+            set;
+            get;
+        }
+
+        public Boolean SoloNumeros
+        {
+            set;
+            get;
+        }
+    }
+}
