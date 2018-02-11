1d4c673597bc7c35efec2eaf152220f45476c736
Ce28 <christian.sapien@hotmail.com>
2018-02-11 11:41:01 -0700

    Se realizo el tutorial 42 del curso

---
+++ Tutorial42/FactuxD/FactuxD/ConsultarClientes.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using MiLibreria;
+
+namespace FactuxD
+{
+    public partial class ConsultarClientes : Consultas
+    {
+        public ConsultarClientes()
+        {
+            InitializeComponent();
+        }
+
+        private void ConsultarClientes_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = LlenarDataGV("Cliente").Tables[0];
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox1.Text.Trim()) == false)
+            {
+                try
+                {
+                    DataSet ds;
+
+                    string cmd = "SELECT * FROM cliente WHERE Nom_cli LIKE ('%" + textBox1.Text.Trim() + "%')";
+                    ds = Utilidades.Ejecutar(cmd);
+                    dataGridView1.DataSource = ds.Tables[0];
+                }
+                catch (Exception error)
+                {
+                    MessageBox.Show("Ha ocurrido un error:" + error.Message);
+                }
+            }
+        }
+    }
+}
